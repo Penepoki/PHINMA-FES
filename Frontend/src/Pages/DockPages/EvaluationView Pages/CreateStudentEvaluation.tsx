1bf406d7e31a52fda28eae0e1f8281505667bf0b
Tinangeli <espinedamartin14@yahoo.com>
2025-04-29 12:50:04 +0800

    added decouple and fixes in front-end

--- Frontend/src/Pages/DockPages/EvaluationView Pages/CreateStudentEvaluation.tsx
+++ Frontend/src/Pages/DockPages/EvaluationView Pages/CreateStudentEvaluation.tsx
@@ -49,13 +49,9 @@ function CreateStudentEvaluation({ setActiveView }: CreateStudentEvalProps) {
       <h2 className="text-3xl font-bold mt-4 text-white">
         Create Student Evaluation
       </h2>
-<<<<<<< Updated upstream
-      <div className="flex w-full justify-center items-start pb-2 px-4 border-b-gray-600 border-b-2 shadow-xl">
-=======
 
       {/* Create New Evaluation Button */}
       <div className="flex w-full justify-center md:justify-end items-start pb-2 px-4 border-b-gray-600 border-b-2 shadow-xl">
->>>>>>> Stashed changes
         <button
           onClick={() =>
             (
