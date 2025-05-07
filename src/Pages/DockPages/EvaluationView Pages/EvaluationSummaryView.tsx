52df1c3bef4a1b406cc9c8361a6fa6089bcaf133
Visionare <renzoscua@gmail.com>
2025-05-07 17:29:23 +0800

    Delete src directory

--- src/Pages/DockPages/EvaluationView Pages/EvaluationSummaryView.tsx
+++
@@ -1,9 +0,0 @@
-function EvaluationSummary() {
-  return (
-    <div className="custom-container text-white">
-      <span className="text-9xl">AI GOES HERE</span>
-    </div>
-  );
-}
-
-export default EvaluationSummary;
