e8d1a0072c89bb4a8708f5cc4d57ccf02be1c3ba
Tinangeli <espinedamartin14@yahoo.com>
2025-07-16 16:50:39 +0800

    requirements.txt ADDED FOR DEPLOYMENT

--- Backend/SaintJudeDjango/settings.py
+++ Backend/SaintJudeDjango/settings.py
@@ -124,9 +124,9 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': config('DB_NAME'),
         'USER': config('DB_USER'),
-        'PASSWORD': config('DB_PASSWORD', default=''),
-        'HOST': config('DB_HOST', default='localhost'),
-        'PORT': config('DB_PORT', default='3306'),      # MySQL default port
+        'PASSWORD': config('DB_PASSWORD'),
+        'HOST': config('DB_HOST'),
+        'PORT': config('DB_PORT'),      # MySQL default port
         'OPTIONS': {
             'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",  # Ensure strict mode
         },
