30132121e79ef9e3ff83461ee789ecc3cff5b5bb
Martin Espineda <espinedamartin14@yahoo.com>
2026-07-15 15:42:57 +0800

    Enable email configuration in settings.py
    Uncomment email configuration settings for SMTP.

--- Backend/SaintJudeDjango/settings.py
+++ Backend/SaintJudeDjango/settings.py
@@ -61,9 +61,9 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-# EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER', '')
-# EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD', '')
-# DEFAULT_FROM_EMAIL = EMAIL_HOST_USER or 'no-reply@phinma-fes.com'
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER', '')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD', '')
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER or 'no-reply@phinma-fes.com'
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 # Application definition
 INSTALLED_APPS = [
